3bb35bca8770106f09de06220b339067c1110b27
netsh3r@gmail.com <netsh3r@gmail.com>
2022-06-21 09:34:27 +0300

    WIP

--- EGTFReader.cs
+++ EGTFReader.cs
@@ -6,5 +6,17 @@ namespace EGTF.Reader
 {
     public class EGTFReader
     {
+        private readonly string url;
+        private readonly Dictionary<string, string> dictValue;
+        public EGTFReader(string url, Dictionary<string, string> dictValue)
+        {
+            this.url = url;
+            this.dictValue = dictValue;
+        }
+
+        public void Create(string createUrl)
+        {
+
+        }
     }
 }
