d3a25cc1aba75241d4370d1ff72d038f54446d8f
Dmitry <76789551+netsh3r@users.noreply.github.com>
2022-06-21 15:02:21 +0400

    refactoring

--- EGTFReader.cs
+++ EGTFReader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
+using ExGTF_Regex = ExGTF.Reader.ExGTF_Const.ExGTF_Regex;
 
 namespace ExGTF.Reader
 {
@@ -10,6 +11,7 @@ namespace ExGTF.Reader
     {
         private readonly string url;
         private readonly Dictionary<string, object> dictValue;
+
         public ExGTFReader(string url, Dictionary<string, object> dictValue)
         {
             this.url = url;
@@ -30,90 +32,29 @@ namespace ExGTF.Reader
                     var rg = new Regex("@(.*)@");
                     newFileName = rg.Match(header).Groups[1].Value;
                 }
+
                 var line = string.Empty;
-                var rgLine = new Regex(@"(<#(\w*)#>)");
-                var rg2 = new Regex(@"(<\$(\w*)\$>)");
-                var argLine = new Regex(@"(\[#(.*)#\])");
-                var argSpLine = new Regex(@"(\[<(.*)>\])");
-                var argSpLineAr = new Regex(@"((\w*):(!{(\d*)..(\d*)}!))(.*)({!(\w*)!})(.*)");
                 while ((line = sr.ReadLine()) != null)
                 {
-                    if (rgLine.IsMatch(line))
+                    if (ExGTF_Regex.SingleLine.IsMatch(line))
                     {
-                        var sp = rgLine.Split(line);
-                        for (int i = 0; i < sp.Length; i++)
-                        {
-                            if (rgLine.IsMatch(sp[i]))
-                            {
-                                sb.AppendLine(dictValue[sp[++i]].ToString());
-                                sb.AppendLine("");
-                            }
-                            else
-                            {
-                                sb.Append(sp[i]);
-                            }
-                        }
+                        AppendSingleLine(line, sb);
                     }
-                    else if (argSpLine.IsMatch(line))
+                    else if (ExGTF_Regex.LocalArrayLine.IsMatch(line))
                     {
-                        var m = argSpLine.Match(line).Groups[2];
-                        var msp = argSpLine.Split(line);
-                        if (argSpLineAr.IsMatch(m.Value))
-                        {
-                            var mm = argSpLineAr.Match(m.Value);
-                            var elName = mm.Groups[2].Value;
-                            for (int i = Int32.Parse(mm.Groups[4].Value); i < Int32.Parse(mm.Groups[5].Value); i++)
-                            {
-                                sb.AppendLine($"{msp[0]}{mm.Groups[6].Value}{i}{mm.Groups[9].Value}");
-                            }
-                        }
+                        AppendLocalArrayLine(line, sb);
                     }
-                    else if (argLine.IsMatch(line))
+                    else if (ExGTF_Regex.ArrayLine.IsMatch(line))
                     {
-                        var m = argLine.Match(line).Groups[2];
-                        var msp = argLine.Split(line);
-                        if (rg2.IsMatch(m.Value))
-                        {
-                            var mr2 = rg2.Match(m.Value);
-                            var sp = rg2.Split(m.Value);
-                            foreach (var val in (Array)dictValue[mr2.Groups[2].Value])
-                            {
-                                sb.AppendLine();
-                                sb.Append($"{msp[0]}");
-                                for (int i = 0; i < sp.Length; i++)
-                                {
-                                    if (rg2.IsMatch(sp[i]))
-                                    {
-                                        sb.Append(val);
-                                        i++;
-                                    }
-                                    else
-                                    {
-                                        sb.Append(sp[i]);
-                                    }
-                                }
-                            }
-                            
-                            sb.AppendLine();
-
-                        }
+                        AppendArrayLine(line, sb);
                     }
-                    else if (rg2.IsMatch(line))
+                    else if (ExGTF_Regex.Single.IsMatch(line))
                     {
-                        sb.AppendLine();
-                        var sp = rg2.Split(line);
-                        for (int i = 0; i < sp.Length; i++)
-                        {
-                            if (rg2.IsMatch(sp[i]))
-                            {
-                                sb.Append(dictValue[sp[++i]]);
-                            }
-                            else
-                            {
-                                sb.Append(sp[i]);
-                            }
-                        }
-                        sb.AppendLine();
+                        AppendSingle(line, sb);
+                    }
+                    else if (line.Contains("[##"))
+                    {
+                        AppendMultiArrayLine(line, sb, sr);
                     }
                     else
                     {
@@ -126,6 +67,100 @@ namespace ExGTF.Reader
             using (var sr2 = new StreamWriter($"{createUrl}/{fileName ?? newFileName}.cs"))
             {
                 sr2.WriteLine(sb.ToString());
+            }
+        }
+
+        private void AppendMultiArrayLine(string line, StringBuilder sb, StreamReader sr)
+        {
+            var lSb = new StringBuilder();
+            var arrayValueName = string.Empty;
+            while ((line = sr.ReadLine()) != null && !line.Contains("##]"))
+            {
+                if (ExGTF_Regex.Single.IsMatch(line))
+                {
+                    if (string.IsNullOrEmpty(arrayValueName))
+                    {
+                        arrayValueName = ExGTF_Regex.Single.Match(line).Groups[2].Value;
+                    }
+                    
+                    AppendSingle(line, lSb, "{0}");
+                }
+                else
+                {
+                    lSb.AppendLine(line);
+                }
+            }
+
+            foreach (var value in (Array)dictValue[arrayValueName])
+            {
+                sb.AppendLine(string.Format(lSb.ToString(), value));
+            }
+        }
+
+        private void AppendSingleLine(string line, StringBuilder sb)
+        {
+            var sp = ExGTF_Regex.SingleLine.Split(line);
+            for (int i = 0; i < sp.Length; i++)
+            {
+                if (ExGTF_Regex.SingleLine.IsMatch(sp[i]))
+                {
+                    sb.AppendLine(dictValue[sp[++i]].ToString());
+                }
+                else
+                {
+                    sb.Append(sp[i]);
+                }
+            }
+        }
+
+        private void AppendLocalArrayLine(string line, StringBuilder sb)
+        {
+            var m = ExGTF_Regex.LocalArrayLine.Match(line).Groups[2];
+            var msp = ExGTF_Regex.LocalArrayLine.Split(line);
+            if (ExGTF_Regex.LocalArrayLineArg.IsMatch(m.Value))
+            {
+                var mm = ExGTF_Regex.LocalArrayLineArg.Match(m.Value);
+                var elName = mm.Groups[2].Value;
+                for (int i = Int32.Parse(mm.Groups[4].Value); i < Int32.Parse(mm.Groups[5].Value); i++)
+                {
+                    sb.AppendLine($"{msp[0]}{mm.Groups[6].Value}{i}{mm.Groups[9].Value}");
+                }
+            }
+        }
+
+        private void AppendArrayLine(string line, StringBuilder sb)
+        {
+            var m = ExGTF_Regex.ArrayLine.Match(line).Groups[2];
+            var msp = ExGTF_Regex.ArrayLine.Split(line);
+            if (ExGTF_Regex.Single.IsMatch(m.Value))
+            {
+                var mr2 = ExGTF_Regex.Single.Match(m.Value);
+                var sp = ExGTF_Regex.Single.Split(m.Value);
+                foreach (var val in (Array)dictValue[mr2.Groups[2].Value])
+                {
+                    sb.AppendLine();
+                    sb.Append($"{msp[0]}");
+                    AppendSingle(m.Value, sb, val);
+                }
+
+                sb.AppendLine();
+            }
+        }
+
+        private void AppendSingle(string line, StringBuilder sb, object value = null)
+        {
+            var sp = ExGTF_Regex.Single.Split(line);
+            for (int i = 0; i < sp.Length; i++)
+            {
+                if (ExGTF_Regex.Single.IsMatch(sp[i]))
+                {
+                    ++i;
+                    sb.Append(value ?? dictValue[sp[i]]);
+                }
+                else
+                {
+                    sb.Append(sp[i]);
+                }
             }
         }
     }
---
+++ ExGTF_Const.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ExGTF.Reader
+{
+    public class ExGTF_Const
+    {
+        public const string EMPTY_FILE_NAME = "@*";
+        public const string START_ARRAY_BLOCK = "[##";
+        public const string END_ARRAY_BLOCK = "##]";
+        
+        public class ExGTF_Regex
+        {
+            public static readonly Regex FileName = new Regex("@(.*)@");
+            
+            /// <summary> Вставка отдельной строки </summary>
+            public static readonly Regex SingleLine = new Regex(@"(<#(\w*)#>)");
+            
+            /// <summary> Вставка значения, без переноса строки </summary>
+            public static readonly Regex Single = new Regex(@"(<\$(\w*)\$>)");
+            
+            /// <summary> Вставка массива значений построчно </summary>
+            public static readonly Regex ArrayLine = new Regex(@"(\[#(.*)#\])");
+            
+            /// <summary> Массив внутренних значений </summary>
+            public static readonly Regex LocalArrayLine = new Regex(@"(\[<(.*)>\])");
+            
+            /// <summary> Аргумент массива внутренних значений </summary>
+            public static readonly Regex LocalArrayLineArg = new Regex(@"((\w*):(!{(\d*)..(\d*)}!))(.*)({!(\w*)!})(.*)");
+        }
+    }
+}
